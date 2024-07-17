3f90f3c318b7f498d5bd016c1aafb8c9e88da7d2
Shaxriyor <shaxriyorturgunboyovshaxriyor@gmail.com>
2024-07-17 05:54:22 +0500

    Mening commitim 17.o7.2024

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const barss = document.querySelector('.bars-y');
 const exits = document.querySelector('.exits');
 
+// Nomber
+const Mic = document.querySelector('.Mic');
+const nol = document.querySelector('.value');
+const Mix = document.querySelector('.Mix');
+
+let a = '1';
+Mix.addEventListener('click', ()=>{
+    
+})
 
 
 
 
 
 
+// Shop / rasm
 const heroMainImg = document.querySelector(".img-type img");
 const asideImgs = document.querySelectorAll(".hero-id img");
 
@@ -16,6 +26,6 @@ asideImgs.forEach((item) => {
     const secondAtt = item.getAttribute("src");
     heroMainImg.setAttribute("src", secondAtt);
     item.setAttribute("src", firstAtt);
-    item.style.Height = "404px"
+    item  .style.Height = "404px"
   });
 });
