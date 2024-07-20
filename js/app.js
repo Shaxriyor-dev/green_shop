d49ee31e9da4ec53708092861fa1f64427a0db95
Shaxriyor <shaxriyorturgunboyovshaxriyor@gmail.com>
2024-07-20 20:24:53 +0500

    Shoping-money

--- js/app.js
+++ js/app.js
@@ -5,6 +5,48 @@ const exits = document.querySelector('.exits');
 const Mic = document.querySelector('.Mic');
 const nol = document.querySelector('.value');
 const Mix = document.querySelector('.Mix');
+const span = document.querySelector('.spans');
+
+nol.addEventListener("click", () => {
+  nol.innerText = 1;
+  Mic.style.display = 'inline-block'
+  Mix.style.display = 'inline-block'
+})
+Mic.addEventListener("click", () => {
+  if (nol.innerText < -1) {
+       alert('Xatolik yuzberdi')
+  } else {
+    nol.innerText = nol.innerText - 1;
+  }
+});
+
+Mix.addEventListener("click", () => {
+  nol.innerText = +(nol.innerText) + +1
+});
+
+
+let count = 199.00;
+Mix.addEventListener('click', ()=>{
+  const cl = span.classList;
+  const c = 'animated'
+  count = count + 199.00
+     span.innerText = count;
+     cl.remove(c , cl.span(c));
+      setTimeout(()=>
+      span.classList.add('animated')
+ ,300)
+})
+Mic.addEventListener('click', ()=>{
+  const cl = span.classList;
+  const c = 'an'
+  count = count - 199.00
+
+     span.innerText = count;
+     cl.remove(c , cl.span(c));
+      setTimeout(()=>
+      span.classList.add('an')
+ ,300)
+})
 
 
 
@@ -22,6 +64,8 @@ asideImgs.forEach((item) => {
     const secondAtt = item.getAttribute("src");
     heroMainImg.setAttribute("src", secondAtt);
     item.setAttribute("src", firstAtt);
-    item  .style.Height = "404px"
+    item.style.Height = "404px"
   });
 });
+
+
